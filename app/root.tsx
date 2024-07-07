aa1b4b51a4ae7069838e8f060190bc252567f5bd
Hamza Malik <37263285+hamza7malik@users.noreply.github.com>
2024-07-07 12:07:27 +0400

    Setup navigation for the application

--- app/root.tsx
+++ app/root.tsx
@@ -4,22 +4,28 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
-import "./tailwind.css";
+} from '@remix-run/react';
+import './tailwind.css';
+import NavigationBar from './components/NavigationBar/NavigationBar';
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
         <Links />
       </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
+      <body className='bg-slate-950 text-white text-center font-sans'>
+        <header>
+          <NavigationBar />
+        </header>
+        <div className='py-8'>
+          {children}
+          <ScrollRestoration />
+          <Scripts />
+        </div>
       </body>
     </html>
   );
